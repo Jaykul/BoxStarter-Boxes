ff40933cac450088a872f904e72e9af260eaf46a
Joel Bennett <Jaykul@HuddledMasses.org>
2023-02-18 20:24:19 -0500

    I don't actually use putty anymore

--- 90_Customize/95_Putty.ps1
+++
@@ -1,5 +0,0 @@
-# Configure git to use plink so that keys from KeeAgent work
-# Some day soon, I should try switching this to the Windows OpenSSH
-choco upgrade -y putty.install
-$plink = Get-Command plink | convert-path
-[System.Environment]::SetEnvironmentVariable("GIT_SSH", $plink, "User")
