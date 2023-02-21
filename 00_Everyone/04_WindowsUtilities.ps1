5186f134a5b58a3f67155f89b04a49d08423e710
Joel Bennett <Jaykul@HuddledMasses.org>
2023-02-21 01:34:01 -0500

    Still trying to set the execution policy on pwsh

--- 00_Everyone/04_WindowsUtilities.ps1
+++ 00_Everyone/04_WindowsUtilities.ps1
@@ -1,7 +1,7 @@
 # Over the years, Microsoft has added built-in screenshot and clipbard tools ...
 # They are not as good as the ones I use, but I won't force my preferences on "everyone"
 choco upgrade -y pwsh --install-arguments='"ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 REGISTER_MANIFEST=1 ENABLE_PSREMOTING=1"'
-pwsh -WindowStyle Hidden -NoProfile -NoLogo -NonInteractive -ExecutionPolicy unrestricted -c "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned"
+Start-Process "pwsh.exe" -verb runas -wait -argumentList "-noprofile -NoLogo -noninteractive -ExecutionPolicy unrestricted -WindowStyle hidden -Command `"Set-ExecutionPolicy RemoteSigned`""
 
 # Everyone definitely needs a better archiver...
 # choco upgrade -y 7zip.install
