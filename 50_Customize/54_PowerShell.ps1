0aabc1eaceea1938ea5b9792a2e83589b0f55608
Joel Bennett <Jaykul@HuddledMasses.org>
2023-02-25 20:12:44 -0500

    Public networks make this fail...

--- 50_Customize/54_PowerShell.ps1
+++ 50_Customize/54_PowerShell.ps1
@@ -1,4 +1,8 @@
-Enable-PSRemoting -Force
+try {
+    Enable-PSRemoting -Force
+} catch {
+    Write-Warning "Failed to enable PSRemoting: $_"
+}
 
 # choco upgrade -y powershell-core
 # choco upgrade -y powershell-preview
