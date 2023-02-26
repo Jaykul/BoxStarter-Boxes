416e48a47afb7217dc4040a68a4afb244b46ee88
Joel Bennett <Jaykul@HuddledMasses.org>
2023-02-25 20:06:30 -0500

    Don't need to mess with the prompt.
    We have chezmoi for that.

--- 50_Customize/54_PowerShell.ps1
+++ 50_Customize/54_PowerShell.ps1
@@ -1,3 +1,5 @@
+Enable-PSRemoting -Force
+
 # choco upgrade -y powershell-core
 # choco upgrade -y powershell-preview
 
@@ -8,10 +10,6 @@
 ) | UpdateModule
 
 Get-MSTerminalProfile | Set-MSTerminalProfile -FontFace "Caskaydia Cove NFM"
-
-Set-PowerLinePrompt -SetCurrentDirectory -PowerLineFont -FullColor -Timestamp -Newline -Save
-
-Enable-PSRemoting -Force
 
 # Push-Location ~\Projects\Modules
 # git clone https://github.com/Jaykul/Profile.git Profile
